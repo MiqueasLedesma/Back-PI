12797be19f5ea61eb1077bd9dbd4e89effc66c35
Miqueas Ledesma <104766422+MiqueasLedesma@users.noreply.github.com>
2022-11-05 16:50:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -20,12 +20,13 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const { InjectVideogames, injectGenres } = require('./src/Injectinfo.js');
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(async () => {
   await injectGenres();
   await InjectVideogames();
-  server.listen(3001, () => {
+  server.listen( PORT || 3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
